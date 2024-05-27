d9ccaa7bfe92b6b67b9ded000119d6b7efdc592e
grunday <veritas1394@gmail.com>
2024-05-27 13:56:20 +0900

    second

---
+++ source/C/ch02_variable/ex2.1_정수형_문자형.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// 주석 OnOff : ctrl +k , c    or ctrl + k , u
+// 변수 : 정수형%d(int) , 실수형(float %f   double %lf     char %c    string %s
+int main(void) {
+
+	int age = 26;
+	char gender = 'M';
+//	char name[] = "성진";;
+	float c = 36.5;
+	double d = 180.1234123214;
+	char* name = "제니";
+	printf("안녕하세요, %d(%c) %s \n", age, gender, name);
+	printf("저의 체온은 %0.1f이고 키는 %0.2lf", c, d);
+	return 0;
+}
